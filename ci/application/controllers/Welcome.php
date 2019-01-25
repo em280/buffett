c17ef6554c6079e223d55ff0c3f5e327a9a0fd9a
Emmanuel Molefi <emmanuelmolefi@medwlanB23A.kent.ac.uk>
2019-01-25 10:28:17 +0000

    Edited the index function ...

--- ci/application/controllers/Welcome.php
+++ ci/application/controllers/Welcome.php
@@ -21,6 +21,6 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-		echo "Hello World! Hello Again!";
+		echo "Hello World! Hello Again! Hello a third time!";
 	}
 }
