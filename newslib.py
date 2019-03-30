1579f676dc3b45df07e08b591376913f10b407c0
E-Molefi <emmanuel.molefi@outlook.com>
2019-03-30 16:42:09 +0000

    Update project

--- newslib.py
+++ newslib.py
@@ -11,17 +11,17 @@ import datetime as dt
 
 def get_general_headlines():
 	result = requests.get("https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=ca4b323cc91f4bf0a4da4f28f70f26f5")
-	
+
 	return result.json()
-	
+
 def search_headlines(company):
 	date = (dt.datetime.now() - dt.timedelta(days=14)).date()
 	base = "https://newsapi.org/v2/everything"
-	
-	payload = {'apiKey' : 'ca4b323cc91f4bf0a4da4f28f70f26f5', 'q' : company, 'sortBy' : 'popularity', 'from' : date} 
-	
+
+	payload = {'apiKey' : 'ca4b323cc91f4bf0a4da4f28f70f26f5', 'q' : company, 'sortBy' : 'popularity', 'from' : date}
+
 	response = requests.get(base, params=payload)
-	
+
 	return response.json()
 
-print(get_general_headlines())
+# print(get_general_headlines())
